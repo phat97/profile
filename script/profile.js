818971e8fefb3b79570c61ec048df887ae8d01c5
Phat <phat.le@hotmail.com>
2019-10-15 18:18:27 -0700

    Added accordion menu to projects section

---
+++ script/profile.js
@@ -0,0 +1,27 @@
+function loadScripts() {
+  displayAccordion();
+}
+
+function displayAccordion() {
+  let acc = document.getElementsByClassName("accordion");
+
+  for (let i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", event => {
+      let targetElement = event.target || event.srcElement;
+      targetElement.classList.toggle("active");
+      let panel = targetElement.nextElementSibling;
+
+      if (targetElement.classList.contains("active")) {
+        panel.style.border = "1px solid black";
+      } else {
+        panel.style.border = "none";
+      }
+
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      }
+    });
+  }
+}
